59c1e51683f8e7bf0c41c076f15d614a1fa729b2
siwolsmu <siwol_smuire89@naver.com>
2020-04-25 15:55:49 +0900

    2020/04/25

---
+++ weekendStudy_04_25/src/controlQuiz/control_3.java
@@ -0,0 +1,37 @@
+package controlQuiz;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 숫자를 입력받아 해당 숫자에 대한 구구단을 출력하기 
+ *  예) 2 입력 시, 2*1부터 2*9까지 (구구단 2단)을 출력
+ * @author siwol_smu
+ *
+ */
+public class control_3 {
+	
+	/**
+	 * 키보드로부터 숫자를 입력받는 메소드
+	 * @return int형 숫자
+	 * @throws IOException 
+	 * @throws NumberFormatException 
+	 */
+	private static int getNumber() throws NumberFormatException, IOException {
+		int result = 0;
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		result = Integer.parseInt(reader.readLine());
+		
+		return result;
+	}
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		System.out.print("숫자를 입력하세요: ");
+		int dan = getNumber();
+		
+		// 해당 숫자에 대한 구구단 출력하기
+	}
+	
+}
