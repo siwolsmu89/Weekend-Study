eb693614afd70095140e77dded5df19318fef4cc
siwolsmu89 <siwol_smuire89@naver.com>
2020-06-06 21:43:48 +0900

    2020/06/06

--- weekendStudy_06_06/test001/NoDuplicate.java
+++ weekendStudy_06_06/test001/NoDuplicate.java
@@ -8,7 +8,7 @@ public class NoDuplicate {
 			int random = (int) (Math.random()*9 + 1);
 			
 			randoms[i] = random;
-			for (int j = 0; j<i; j++) {
+			for (int j = 0; j<i; j++) { 
 				if (randoms[j]==randoms[i]) {
 					i--;
 					System.out.println("["+(j+1)+"]번째 숫자와 중복이 발생해 숫자를 다시 뽑습니다.");
